97f1e3de58b15fab6b3455b84b400340cb129fdb
antwongreen <antwon_green@hotmail.com>
2014-04-08 19:47:26 -0400

    adding bootstrap

---
+++ WebContent/my1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+ $('kitties').click(function)	
+ 
+ 
+function showPuppies() 
+var pic_div = document.getElementById('pic')
+pic_div.inner
+{
+
+}
+
+	function showkitties(){
+		var pic_div = document.getElementById('pic')
+		}
